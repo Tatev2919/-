2b4d8f1a1353cb211fe3f5f598fad904bbd7ad20
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-05-28 15:09:41 +0400

    Update Oslik:D.py

--- Oslik:D.py
+++ Oslik:D.py
@@ -1,13 +1,13 @@
 n = int(input())
 while True:
-  str = input()
-  if len(str) == 0:
-    break
-  if str[0].isupper() :
-    if str[-1] == '?':
-      n = n - 5 
-    elif (str[-1] == '.') or (str[-1] == '!'):
-      n = n - 3 
+    str = input()
+    if len(str) == 0:
+        break
+    if str[0].isupper():
+        if str[-1] == '?':
+            n = n - 5
+        elif (str[-1] == '.') or (str[-1] == '!'):
+            n = n - 3
 if n < 0:
-  n = 0
+    n = 0
 print(n)
